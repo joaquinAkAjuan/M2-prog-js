e4a8df08302c85e08d5e1a85c64662adb153638d
Joaquin de Boer <joaquindeboer@Joaquins-MacBook-Pro.local>
2022-11-21 10:22:54 +0100

    toevoegen

---
+++ les1/app.js
@@ -0,0 +1,26 @@
+class App
+{
+        runApplication()
+        {
+            console.log("hello world!");
+
+
+            let appNaam = "Shinra tensei";
+            let versienummer = 1.1;
+            let versiedatum = Date;
+            let autheur = "Joaquin de Boer";
+            let copyright = "k hoek je moer";
+            let distributeur = "Joaquins laptop";
+            let darkmode = true;
+
+            console.log(appNaam);
+            console.log(versienummer);
+            console.log(versiedatum);
+            console.log(autheur);
+            console.log(copyright);
+            console.log(distributeur);
+            console.log(darkmode);
+        }
+}
+let app = new App();
+app.runApplication();
