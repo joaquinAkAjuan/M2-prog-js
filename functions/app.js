53e3630aeeee1244cb0028e404efe9cc4883238d
joaquinAkAjuan <112825010+joaquinAkAjuan@users.noreply.github.com>
2022-11-28 11:20:52 +0100

    les2 toevoegen

---
+++ functions/app.js
@@ -0,0 +1,62 @@
+
+// class App
+// {
+
+//     runApplication()
+//     {
+//         console.log("hello world!");
+
+
+//         let appNaam = "Shinra tensei";
+//         let versienummer = 1.1;
+//         let versiedatum = Date;
+//         let autheur = "Joaquin de Boer";
+//         let copyright = "k hoek je moer";
+//         let distributeur = "Joaquins laptop";
+//         let darkmode = true;
+
+//         console.log(appNaam);
+//         console.log(versienummer);
+//         console.log(versiedatum);
+//         console.log(autheur);
+//         console.log(copyright);
+//         console.log(distributeur);
+//         console.log(darkmode);
+//     }
+    
+
+// }
+// let app = new App();
+// app.runApplication();
+
+
+class App
+{
+    
+    
+    runApplication()
+    {
+        this.callmario();
+        this.returnbwa();
+    }
+    
+
+    callmario()
+    {
+        console.log("Mario!!!")
+    }
+        
+
+    returnbwa()
+    {
+        let delach = "BWAHAHAHA";
+        return delach
+        
+    }
+
+    
+
+
+}
+let app = new App();
+app.runApplication();
